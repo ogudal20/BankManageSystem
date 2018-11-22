b9c37e77b10cb8a1fc7019e1c87590e03c12803a
ogudal20 <omar_gudal@hotmail.co.uk>
2018-11-22 23:25:28 +0000

    trouble with login system

---
+++ bankManagementSystem.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define FILENAME "loginDetails.txt"
+char buffer[255];
+
+/* Create Account*/
+typedef struct{
+    char *firstname;
+    char *lastname;
+    char *address;
+    char *password;
+    int mobileNum;
+    int dateOfBirth;
+    int accountNum;
+    int sortCode;
+} userAccount;
+
+
+/* Login Account */
+typedef struct{
+    char *userName;
+    char *password;
+} Login;
+
+
+int mainMenu(void);
+void login();
+void createAccount(userAccount *userDetails);
+void changeUserDets(userAccount *userDetails);
+float deposit(int balance);
+float withdraw(int balance);
+void viewCustomerBankInfo(userAccount *account);
+int editCustomerBankInfo(userAccount *customDetails);
+int delCustomerBankInfo(userAccount *customDetails);
+
+
+int main(void){
+    
+    int choice;
+
+    printf("---Bᴀɴᴋ Mᴀɴᴀɢᴇᴍᴇɴᴛ Sʏsᴛᴇᴍ---\n");
+
+    do{
+        choice = mainMenu();
+        FILE *fp;
+        fp = fopen(FILENAME, "r");
+        
+        if(!fp){
+            printf("Error opening file!");
+            return -1;
+        }
+        
+        /*
+            fscanf(fp, "%s", username);
+           // fgets(password, sizeof(buffer), fp);
+            if(strcmp(username, myUsername) == 0){
+                printf("Correct!\n");
+                break;    
+            }else{
+                printf("Wrong!\n");
+                continue;
+            }
+
+        }while(!feof(fp));
+          
+        */
+        
+        switch(choice){
+            case 1:
+                login();
+                break;
+            case 2:
+           //     createAccount(userAccount *userDetails);
+                break;
+            default:
+                exit(0);
+                break;
+        }
+        
+
+    }while(choice != 3);
+        
+    return 0;
+}
+
+int mainMenu(){
+
+    char userChoice[10];
+
+    printf("1 - Login\n");
+    printf("2 - Create Account\n");
+    printf("3 - Exit\n");
+    printf("Enter a option\n");
+
+    fgets(userChoice, 10,  stdin);
+    int choice = atoi(userChoice);
+
+    if(choice == 1){ return choice; }
+    else if(choice == 2){ return choice;}
+    else{ choice;}
+
+}
+ 
+//Get the login details and store in a file.
+void login(){
+
+
+    FILE *loginDetails;
+
+    char userNameFile[100];
+    char passwordFile[100];
+
+    loginDetails = fopen("loginDetails.txt", "r");
+
+    Login *login = malloc(sizeof(*login));
+
+
+    login->userName = malloc(sizeof(*login->userName));
+    login->password = malloc(sizeof(*login->password));
+
+    printf("Enter in UserName: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    sscanf(buffer, "%s", login->userName);
+
+    printf("Enter in PassWord: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    sscanf(buffer, "s", login->password);
+    
+    //Check to see if the credntials are correct.
+    while(!feof(loginDetails)){
+        fscanf(loginDetails, "%s %s",userNameFile, passwordFile);
+        if((strcmp(userNameFile, login->userName)) == 0 && (strcmp(passwordFile, login->password)) == 0){
+            printf("Correct!\n");
+            break;
+        }else{
+            printf("Wrong\n");
+            printf("%s %s", userNameFile, passwordFile);
+            break;
+        }
+    }
+
+    free(login);
+    free(login->userName);
+    free(login->password);
+
+    return;
+}
+
+
+
+
+
